4a8c07e893f00aafb9f9bdc65880a3f27d0959a6
TrejoJulian <trejojulian998@gmail.com>
2025-03-30 20:36:32 -0300

    Project scaffolding with login connected with backend for project connection test

---
+++ src/components/landing/HeroWidget.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        id="hero"
+        class="flex flex-col pt-6 px-6 lg:px-20 overflow-hidden"
+        style="background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, rgb(238, 239, 175) 0%, rgb(195, 227, 250) 100%); clip-path: ellipse(150% 87% at 93% 13%)"
+    >
+        <div class="mx-6 md:mx-20 mt-0 md:mt-6">
+            <h1 class="text-6xl font-bold text-gray-900 leading-tight">Track & Shop with Ease</h1>
+            <p class="font-normal text-2xl leading-normal md:mt-4 text-gray-700">Follow your purchases in real time and shop smarter with <span class="font-bold text-gray-900 leading-tight">Follow Your Purchases</span>. Stay updated, stay in control! </p>
+            <Button label="Get Started" as="router-link" to="/" rounded class="!text-xl mt-8 !px-4"></Button>
+        </div>
+        <div class="flex justify-center md:justify-end">
+            <img src="https://www.leadsquared.com/wp-content/uploads/2020/04/Sales-tracking.png" alt="Hero Image" class="w-9/12 md:w-auto" />
+        </div>
+    </div>
+</template>
